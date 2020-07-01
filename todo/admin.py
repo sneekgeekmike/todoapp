d32545b6746cd885f54971a72ea91111d8560697
sneekgeekmike <ajibewaibukun@yahoo.com>
2020-07-01 20:04:24 +0400

    Models update

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Task
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Task,TaskAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Category(models.model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name #name to be shown when called
+
+
+    
+    
+    
+    
+class Task(models.Model):
+    title = models.CharField(max_length = 250, blank = True)
+    description = models.CharField(max_length = 200)
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))  # a date
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+    category = models.ForeignKey(Category, default = "general", on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title # name to be shown when called 
